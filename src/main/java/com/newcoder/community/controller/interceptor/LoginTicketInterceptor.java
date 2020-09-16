759861515886496533e192b4ad9eebda0d8d43ab
912498114xgr <912498114@qq.com>
2020-09-16 21:40:59 +0800

    rafeal：新增拦截器、请求获取cookie得到用户登录凭证。从而得到用户信息，并将其设置到模板引擎中。

---
+++ src/main/java/com/newcoder/community/controller/interceptor/LoginTicketInterceptor.java
@@ -0,0 +1,62 @@
+package com.newcoder.community.controller.interceptor;
+
+import com.newcoder.community.entity.LoginTicket;
+import com.newcoder.community.entity.User;
+import com.newcoder.community.service.UserService;
+import com.newcoder.community.util.CookieUtil;
+import com.newcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Date;
+
+//每次走接口请求的时候都去拦截
+@Component
+public class LoginTicketInterceptor implements HandlerInterceptor {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    //判断cookie传来的登陆凭证查数据里的凭证信息是否有效或超时，如果有效根据凭证的用户id查到User。
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        // 从cookie中获取登录凭证
+        String ticket = CookieUtil.getValue(request, "ticket");
+        if (ticket != null) {
+            // 查询凭证
+            LoginTicket loginTicket = userService.findLoginTicket(ticket);
+            // 检查凭证是否有效
+            if (loginTicket != null && loginTicket.getStatus() == 0 && loginTicket.getExpired().after(new Date())) {
+                // 根据凭证查询用户
+                User user = userService.findUserById(loginTicket.getUserId());
+                // 在本次请求中持有用户
+                hostHolder.setUser(user);
+            }
+        }
+
+        return true;
+
+    }
+    //设置对象信息到视图里
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+        User user = hostHolder.getUser();
+        if (user != null && modelAndView != null) {
+            modelAndView.addObject("loginUser", user);
+        }
+    }
+    //使用完之后清楚
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        hostHolder.clear();
+    }
+
+
+}
