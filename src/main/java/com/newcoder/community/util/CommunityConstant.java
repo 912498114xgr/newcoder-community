3bc2bfeb3b0848984f96250b991727342cb71d5d
912498114xgr <912498114@qq.com>
2020-09-17 10:07:33 +0800

    rafeal：增加显示帖子评论

--- src/main/java/com/newcoder/community/util/CommunityConstant.java
+++ src/main/java/com/newcoder/community/util/CommunityConstant.java
@@ -16,6 +16,7 @@ public interface CommunityConstant {
      * 激活失败
      */
     int ACTIVATION_FAILURE = 2;
+
     /**
      * 默认状态的登录凭证的超时时间
      */
@@ -26,5 +27,14 @@ public interface CommunityConstant {
      */
     int REMEMBER_EXPIRED_SECONDS = 3600 * 24 * 100;
 
+    /**
+     * 实体类型: 帖子
+     */
+    int ENTITY_TYPE_POST = 1;
+
+    /**
+     * 实体类型: 评论
+     */
+    int ENTITY_TYPE_COMMENT = 2;
 
 }
